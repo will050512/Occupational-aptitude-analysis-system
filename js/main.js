340cf82299b77083d8dd0f48edacd15f2d4b7efb
root <info.aifunschool@gmail.com>
2025-11-28 10:39:49 +0800

    01

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import { GameEngine } from './engine.js';
-import { socialStylesModule } from './modules/socialStyles.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const engine = new GameEngine('game-container');
-    
-    // Load the default module
-    engine.loadModule(socialStylesModule);
-});
